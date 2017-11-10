3e012db6fb91c02768f472af3ab53e52b38e8d1b
Craig Simons <craigsimons@sfu.ca>
2017-11-09 23:52:02 -0800

    Added ability to parameterize variables.

--- bulky.py
+++ bulky.py
@@ -2,27 +2,87 @@ import paramiko
 import getpass
 import socket
 import argparse
+import csv
+import re
+import sys
 from argparse import RawTextHelpFormatter
 
 def main():
 	# get command line arguments
 	args = getArgs()
-
-	user = raw_input("Enter your username:")
-	password = getpass.getpass("Enter your password:")
-	args = getArgs()
 	hosts = getHosts(args)
+	# get command
+	command = getCommand(args)
+	varCount = getCommandVarCount(command)
 
-	for host in hosts:
-		ssh(host, user, password, args.command)
+	if not args.test:
+		user = raw_input("Enter your username:")
+		password = getpass.getpass("Enter your password:")	
+
+	for line in hosts:
+		# Read each CSV line and arrange into host and variable array
+		thisLine = csv.reader([line])
+		columns = list(thisLine)[0]
+		host = columns[0]
+		columns.pop(0)
+		columnCount = len(columns)
+
+		# test if variables in each coc
+		if varCount != len(columns):
+			print "There is a problem with: " + str(line)
+			print "Number of variables in supplied command was " + str(varCount) + " but the line had " + str(columnCount) + " columns"
+			sys.exit(1)
+
+		if not args.test:
+			ssh(host, user, password, replaceVariables(columns, command))
+		else:
+			printBanner(host, command)
+
+def replaceVariables(variables, command):
+	"""
+	Replaces all variables in the format {{integer}}
+	"""
+	for i, value in enumerate(variables):
+		command = command.replace("{{" + str(i) + "}}",variables[i])
+
+	return command
 
 def getHosts(args):
-
+	""" 
+	Reads CSV file in 
+	"""
 	if args.file:
 		with open(args.file) as f:
 			return f.read().splitlines()
 
 	return args.list.split(',')
+
+def getCommand(args):
+	if args.commandfile:
+		command = open(args.commandfile, 'r').read()
+		return parseCommand(command)
+	elif args.command:
+		return args.command
+	else:
+		print "No command or command file specified"
+		sys.exit(1)
+
+def parseCommand(command):
+	result = []
+	commands = command.split('\n')
+	for line in commands:
+		if line:
+			result.append(line)
+	return "; ".join(result)
+
+def getCommandVarCount(command):
+	""" 
+	Get the number of variables in the command. This number will be used to test each host entry
+	"""
+	if command:
+		return len(re.findall('\{\{.*?\}\}', command))
+	else:
+		return 0
 
 def getArgs():
 	"""
@@ -36,12 +96,22 @@ def getArgs():
 	parser = argparse.ArgumentParser(description=desc, formatter_class=RawTextHelpFormatter)
 	parser.add_argument("-l", "--list", help="Comma separated list of ip addresses or switches to do stuff on.", required=False)
 	parser.add_argument("-f", "--file", help="CSV file hosts and variables. Must be delimited with quotes.", required=False)
-	parser.add_argument("-c", "--command", help="Command to issue", required=True)
-	parser.add_argument("-t", "--test", help="Run in test mode only without actually connecting to the host.")
+	parser.add_argument("-c", "--command", help="Command to issue", required=False)
+	parser.add_argument("-cf", "--commandfile", help="Command file to load", required=False)
+	parser.add_argument("-t", "--test", help="Run in test mode only without actually connecting to the host.", action="store_true")
 
 	result = parser.parse_args()
 
 	return result
+
+def printBanner(host, command):
+		print ""
+		print "#####################"
+		print " From " + host
+		print "#####################"
+		print ""
+		print "Command: " + command
+		print ""
 
 def ssh(host, user, password, command):
 	ssh = paramiko.SSHClient()
@@ -51,11 +121,7 @@ def ssh(host, user, password, command):
 		ssh.connect(host, username=user, password=password)
 		stdin,stdout,stderr= ssh.exec_command(command)
 
-		print ""
-		print "#####################"
-		print " From " + host
-		print "#####################"
-		print ""
+		printBanner(host, command)
 		for line in stdout:
 			print line.strip('\n')
 
